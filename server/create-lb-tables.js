380f7aed145c3f76d56223d50ce0164687919e10
Camilo Velasquez <camilo.velasquezp@gmail.com>
2019-03-31 13:51:51 -0500

    Se ajusta nuevo modelo de formulario

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,6 @@
 var server = require('./server');
 var ds = server.dataSources.db;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'menu', 'data_form'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
